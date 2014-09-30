b3cb74f92fda630733a3157da609aac5e2b97145
Thibault BEZIERS-LA-FOSSE <E104607D@S040V7pc18.ensinfo.sciences.univ-nantes.prive>
2014-09-30 15:48:45 +0200

    warrior class added

---
+++ Warrior.cpp
@@ -0,0 +1,3 @@
+Warrior::Warrior() {
+	Character(
+}
---
+++ Warrior.hpp
@@ -0,0 +1,13 @@
+
+#ifndef WARRIOR_HPP
+#define WARRIOR_HPP
+
+class Warrior : public Player {
+
+	public:
+		Warrior();
+		~Warrior();
+
+};
+
+#endif
